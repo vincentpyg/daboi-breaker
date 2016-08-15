0421ea7f6222c9ab84c707772fafaffdd8166cff
vincentg <vincentg.work@gmail.com>
2016-08-15 21:01:36 +0800

    initial commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+    public void loadLevel(string level) {
+        print("loading Level: " + level);
+        Application.LoadLevel(level);
+    }
+   
+    public void quit(string level) {
+        print("quitting application");
+        Application.Quit();
+    }
+} 
